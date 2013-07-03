00e95a65e150b13bea118daa88e08b2e4f1136b2
Abhishek Anand <abhishek.anand.iitg@gmail.com>
2013-07-03 12:14:27 -0400

    added file type and a dummy proof window

---
+++ src/coq/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@TemplateRegistration(folder = "Other", content = "cqTemplate.v")
+package coq;
+
+import org.netbeans.api.templates.TemplateRegistration;
